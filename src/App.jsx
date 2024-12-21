02dd079df96223769ca8c8fc529ac0890bac345f
Datech2024 <dawitwelene@gmail.com>
2024-12-20 22:05:12 -0500

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -10,43 +10,77 @@ import Signup from "./Pages/Signup";
 export const AppState = createContext();
 
 function App() {
-  const [user, setUser] = useState({});
-  const navigate = useNavigate();
-  const token = localStorage.getItem("token");
+	const [user, setUser] = useState({});
+	const navigate = useNavigate();
+	const token = localStorage.getItem("token");
 
-  async function checkUser() {
-    try {
-      const { data } = await axios.get("/users/check", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setUser(data);
-    } catch (error) {
-      console.log(error.response?.data?.message || "An error occurred");
+	async function checkUser() {
+		try {
+			const { data } = await axios.get("/users/check", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			setUser(data);
+		} catch (error) {
+			console.log(error.response?.data?.message || "An error occurred");
 
-      navigate("/login");
-    }
-  }
+			navigate("/login");
+		}
+	}
 
-  useEffect(() => {
-    checkUser();
-  }, []);
+	useEffect(() => {
+		checkUser();
+	}, []);
 
-  return (
-    <>
-      <AppState.Provider value={{ user, setUser }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/question" element={<AskQuestion />} />
-        </Routes>
-        <Footer />
-      </AppState.Provider>
-      
-    </>
-  );
+	return (
+		<>
+			<AppState.Provider value={{ user, setUser }}>
+				<Routes>
+					<Route
+						path="/home"
+						element={
+							<>
+								{/* <Header /> */}
+								<Home />
+								<Footer />
+							</>
+						}
+					/>
+					<Route
+						path="/signup"
+						element={
+							<>
+								{/* <Header/> */}
+								<Signup />
+								<Footer />
+							</>
+						}
+					/>
+					<Route
+						path="/login"
+						element={
+							<>
+								{/* <Header /> */}
+								<Login />
+								<Footer />
+							</>
+						}
+					/>
+					<Route
+						path="/question"
+						element={
+							<>
+								{/* <Header /> */}
+								<AskQuestion />
+								<Footer />
+							</>
+						}
+					/>
+				</Routes>
+			</AppState.Provider>
+		</>
+	);
 }
 
 export default App;
